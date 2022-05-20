495392c09aa3be2b07f8a9e3169d5235b92b3f3a
Nathan Vitor <nathan@nathanpw12.localdomain>
2022-05-20 20:00:02 -0300

    Requisito 3: Aprovado Localmente

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,17 @@
+// const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (employeeName === undefined) {
+    return {};
+  }
+
+  if (employeeName.length > 0) {
+    return (
+      data.employees.find((element) => element.firstName === employeeName)
+      || data.employees.find((element) => element.lastName === employeeName)
+    );
+  }
 }
 
 module.exports = getEmployeeByName;
