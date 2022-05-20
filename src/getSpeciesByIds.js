a2cdaee4b19bbf57936a69e8c20aec7215bacf33
Nathan Vitor <nathan@nathanpw12.localdomain>
2022-05-20 18:38:10 -0300

    Requisito 1: Aprovado Localmente

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,11 @@
+// const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  if (ids.length === 0) {
+    return ids;
+  }
+  return data.species.filter((element) => ids.includes(element.id));
 }
 
 module.exports = getSpeciesByIds;
