75f1e154450f01cb745e317d0e5db4c10844bec5
Nathan Vitor <nathanvitor38@gmail.com>
2022-06-02 19:45:16 -0300

    Requisito 5: Aprovado Localmente

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,8 +1,24 @@
-// const { species } = require('../data/zoo_data');
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // a
+  if (!animal) {
+    return species.reduce(
+      (acc, { name, residents }) => ({ ...acc, [name]: residents.length }),
+      {},
+    );
+  }
+
+  const { specie, sex } = animal;
+
+  if (specie && sex) {
+    return species
+      .find(({ name }) => name === specie)
+      .residents.filter(({ sex: sexKey }) => sexKey === sex).length;
+  }
+
+  if (specie) {
+    return species.find(({ name }) => name === specie).residents.length;
+  }
 }
 
 module.exports = countAnimals;
