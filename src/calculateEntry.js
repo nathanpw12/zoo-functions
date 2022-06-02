62c9d8db698d901bf9198bd553d1734cb87e0f23
Nathan Vitor <nathanvitor38@gmail.com>
2022-06-02 14:58:58 -0300

    Requisito 8: Aprovado Localmente

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,28 @@
 const data = require('../data/zoo_data');
 
+const { prices } = data;
+
 function countEntrants(entrants) {
-  // seu código aqui
+  const childs = entrants.filter((element) => element.age < 18);
+  const adult = entrants.filter(
+    (element) => element.age >= 18 && element.age < 50,
+  );
+  const senior = entrants.filter((element) => element.age >= 50);
+
+  const totalEntrantsByAge = {
+    child: childs.length,
+    adult: adult.length,
+    senior: senior.length,
+  };
+  return totalEntrantsByAge;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants || Object.keys(entrants).length === 0 || undefined) {
+    return 0;
+  }
+  const { child, adult, senior } = countEntrants(entrants);
+  return child * prices.child + adult * prices.adult + senior * prices.senior;
 }
 
 module.exports = { calculateEntry, countEntrants };
