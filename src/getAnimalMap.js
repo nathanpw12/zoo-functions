919274f440e984d176e1ef47602fb28f5b36e339
Nathan Vitor <nathanvitor38@gmail.com>
2022-06-03 10:26:41 -0300

    Requisito 11: Aprovado Localmente

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,52 @@
 const data = require('../data/zoo_data');
 
-function getAnimalMap(options) {
-  // seu código aqui
+const sortedResidents = (residents, sorted, sex) => {
+  if (sex && sorted) {
+    return residents
+      .filter(({ sex: residentSex }) => sex === residentSex)
+      .map(({ name }) => name)
+      .sort();
+  }
+
+  if (sorted) return residents.map(({ name }) => name).sort();
+
+  if (sex) {
+    return residents
+      .filter(({ sex: residentSex }) => sex === residentSex)
+      .map(({ name }) => name);
+  }
+
+  return residents.map(({ name }) => name);
+};
+
+const searchSpecies = () =>
+  data.species.reduce(
+    (acc, { name, location }) => ({
+      ...acc,
+      [location]: [...(acc[location] || []), name],
+    }),
+    {},
+  );
+
+const findSpecies = (sorted, sex) =>
+  data.species.reduce(
+    (acc, { name, location, residents }) => ({
+      ...acc,
+      [location]: [
+        ...(acc[location] || []),
+        {
+          [name]: sortedResidents(residents, sorted, sex),
+        },
+      ],
+    }),
+    {},
+  );
+
+function getAnimalMap(param) {
+  if (!param) return searchSpecies();
+  const { includeNames, sorted, sex } = param;
+  if (!includeNames) return searchSpecies();
+  return findSpecies(sorted, sex);
 }
 
 module.exports = getAnimalMap;
